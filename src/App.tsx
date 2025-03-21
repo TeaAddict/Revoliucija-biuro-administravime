6a1ab4a08afbcfdf751af172250a2f64a048dfc2
Deividas Morkunas <deividasmor7@gmail.com>
2025-03-21 13:28:59 +0400

    add svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Computer from "./assets/computer.svg?react";
 
 function App() {
 
   return (
     <div className='bg-orange-500'>
       <h1>Revoliucija!</h1>
+      <Computer className={""}/>
     </div>
   )
 }
