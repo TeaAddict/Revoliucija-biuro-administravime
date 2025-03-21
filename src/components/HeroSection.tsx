efc3e53eb33e479792a6be2b9fab3831a012e954
Laima <kulbyte.laima@gmail.com>
2025-03-21 13:40:56 +0200

    update on hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,12 +1,14 @@
 const HeroSection = () => {
   return (
     <div className="h-[50vh] relative">
-      <img src="src/assets/inf.webp" className="w-full h-full object-cover" />
-
+      <div className="bg-linear-to-t from-orange-950/90 h-full w-full absolute"></div>
+      <div className="bg-linear-to-t from-orange-950/90 h-full w-full absolute"></div>
+      <img src="src/assets/sculpture1.webp" className="w-full h-full object-cover " />
+      
       <div className="flex flex-col gap-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-        <h2 className="text-5xl text-white">Forumas</h2>
-        <p className="text-2xl text-white">
-          "(R)Evoliucija biuro administravime: patirtys, nauji įgūdžiai ir
+        <h2 className="text-5xl text-white font-first tracking-wider">Forumas</h2>
+        <p className="text-2xl text-white font-first tracking-wider">
+          "(r)Evoliucija biuro administravime: patirtys, nauji įgūdžiai ir
           iššūkiai"
         </p>
       </div>
