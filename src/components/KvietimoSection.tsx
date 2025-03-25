710d0046b2a76f9ad9615b2b22743f2a696cc8ac
Laima <kulbyte.laima@gmail.com>
2025-03-25 15:33:18 +0200

    add pattern background

--- src/components/KvietimoSection.tsx
+++ src/components/KvietimoSection.tsx
@@ -1,7 +1,8 @@
 const KvietimoSection = () => {
   return (
-    <div className="flex justify-center py-20 text-white">
-      <div className="flex flex-col gap-32 bg-first p-10 rounded-md">
+    <div className="flex justify-center py-20 text-white relative">
+      <div className="flex flex-col bg-first p-10 rounded-md">
+        
         <div className="flex flex-col gap-5 text-center">
           <h1 className="text-2xl text-sixth">FORUMAS</h1>
           <h2 className="font-bold text-2xl">
@@ -12,18 +13,35 @@ const KvietimoSection = () => {
 
         <div className="flex flex-col gap-6">
           <div>
-            <p className="flex justify-end">
-              2025 m. balandžio 16 diena, 10:00
-            </p>
-            <p>
+          <p className="font-bold pt-5 text-sixth">
+            Maloniai kviečiame atvykti ir pasidalinti mintimis ir įžvalgomis.{" "}
+          </p>
+            
+            <p className="flex justify-center pt-5">
               TECHIN Aviacijos ir verslo administravimo skyriuje (Lakūnų g. 3,
               Vilnius).
             </p>
           </div>
-          <p className="font-bold text-center">
-            Maloniai kviečiame atvykti ir pasidalinti mintimis ir įžvalgomis.{" "}
-          </p>
+          <p className="flex justify-end">
+              2025 m. balandžio 16 diena, 10:00
+            </p>
         </div>
+        
+        <div className="flex items-end">
+          {/* <div className="w-60 h-60 bg-chevron"></div> */}
+
+          <div className="flex ">
+            <div className="w-32 h-44 bg-chevron-reverse"></div>
+            <div className="w-32 h-44 bg-chevron"></div>
+          </div>
+          <div className="flex items-end">
+            <div className="w-32 h-24 bg-chevron-reverse"></div>
+            <div className="w-60 h-24 bg-chevron"></div>
+            <div className="w-60 h-16 bg-chevron-reverse"></div>
+            
+          </div>
+        </div>
+
       </div>
     </div>
   );
