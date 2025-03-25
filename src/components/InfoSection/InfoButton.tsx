553ce478d068b1d736642ca1b8c7e015caf915e8
Laima <kulbyte.laima@gmail.com>
2025-03-25 13:55:07 +0200

    icons update

--- src/components/InfoSection/InfoButton.tsx
+++ src/components/InfoSection/InfoButton.tsx
@@ -1,12 +1,14 @@
 interface CardProps {
   img: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  // img: React.ReactElement;
   name: string;
+  style?: string;
 }
 
-const InfoButton: React.FC<CardProps> = ({ img: Img, name }) => {
+const InfoButton: React.FC<CardProps> = ({ img: Img, name, style }) => {
   return (
     <div className="flex flex-col text-center p-5 rounded-xl gap-2 bg-fourth hover:bg-second hover:outline hover:outline-first">
-      <Img className={"w-full h-12 fill-current text-[#2BD47A]"} />
+      <Img className={`w-full h-12 text-[#2BD47A] ${style}`} />
       <div className="flex flex-col">
         {/* <p className="font-[600] text-xl text-[#2BD47A]">{year}</p> */}
         <p className="text-[#2BD47A]">{name}</p>
